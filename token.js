331d5aa81e5c002fa92f9d40b085a9951b465aea
evellyn.s@escolar.ifrn.edu.br <evellyn.r@academico.ifrn.edu.br>
2025-12-01 11:46:55 -0300

    trabalhando com themes

---
+++ token.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily : {
+                'sans' : ['Roboto', 'system-ui', 'sans-serif'],
+            },
+            colors   : {
+                // text-ifrn-green
+                // text-ifrn-red
+                'ifrn'   : {
+                    'green' : {
+                        50  :  '#f3fbf2',
+                        100  : '#e2f7e1',
+                        200  : '#c8edc5',
+                        300  : '#9bde97',
+                        400  : '#68c662',
+                        500  : '#41ab3c',
+                        600 : '#359830',
+                        700  : '#296f26',
+                        800 :  '#255823',
+                        900  : '#1f491e',
+                        950 : '#0c270c',
+                    },
+                    'red'  :  {
+                        50  :  '#fff1f0',
+                        100  : '#ffe0dd',
+                        200  : '#ffc6c1',
+                        300  : '#ff9f96',
+                        400  : '#ff685a',
+                        500  : '#ff3a27',
+                        600    : '#fb1c07',
+                        700  : '#fb1c07',
+                        800 :  '#af1405',
+                        900  : '#90170c',
+                        950 : '#4f0700',
+
+                    }
+                }
+            }   
+        }
+    }
+}
